b3499c6658b8358e6f55f76b807bd43fff71ac02
Clément <clementprevost45@gmail.com>
2022-07-22 22:53:34 +0200

    style v1 finish and first action 'base' on pop up etc...

---
+++ app/developpement/js/actionOpenMenu.js
@@ -0,0 +1,39 @@
+const body = document.querySelector('body');
+
+const bouton_addList = document.querySelector("#add_list_btn");
+const menu_addList = document.querySelector('#ajoutList-menu');
+
+if ( bouton_addList != undefined ) {
+
+    bouton_addList.addEventListener('click', function(){
+
+        menu_addList.classList.toggle("active");
+    
+    })
+
+}
+
+const containerPopUp = document.querySelector('#containerPopUp');
+const popUp_Projet = document.querySelector('#containerPopUp .add_project_popUp');
+const btn_popUp_Projet = document.querySelector('#add_project')
+const btn_Close_PopUp_projet = document.querySelector('#AddcloseProject');
+
+if ( btn_popUp_Projet != undefined ) {
+
+    btn_popUp_Projet.addEventListener('click', function(){
+
+        body.style.overflow = "hidden"
+        containerPopUp.classList.add('active')
+        popUp_Projet.classList.add('active')
+
+    })
+
+    btn_Close_PopUp_projet.addEventListener('click', function(){
+
+        body.style.overflow = "auto"
+        containerPopUp.classList.remove('active')
+        popUp_Projet.classList.remove('active')
+
+    })
+
+}
